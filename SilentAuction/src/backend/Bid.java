fe1b6b13c9498e573786fefc8d19b6ba36ce64ec
Chris Ottersen <christopherottersen@gmail.com>
2015-05-19 17:31:52 -0700

    commit

---
+++ SilentAuction/src/backend/Bid.java
@@ -0,0 +1,14 @@
+package backend;
+
+public class Bid implements Contribution
+{
+  private final double amount;
+  private final int bidderId;
+  public Bid(double amount, int bidderId)
+  {
+    this.amount = amount;
+    this.bidderId = bidderId;
+  }
+  
+
+}
---
+++ SilentAuction/src/backend/Cash.java
@@ -0,0 +1,12 @@
+package backend;
+
+public class Cash implements Contribution
+{
+  private final double value;
+  public Cash(final double value)
+  {
+    this.value = value;
+  }
+  
+  
+}
