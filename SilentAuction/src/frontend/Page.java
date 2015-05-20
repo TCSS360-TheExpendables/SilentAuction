77e7205855aec636a43dfa988a346170e104597d
HSilva4 <hsilva@uw.edu>
2015-05-19 19:51:34 -0700

    Added components to registration page

--- SilentAuction/src/frontend/Page.java
+++ SilentAuction/src/frontend/Page.java
@@ -19,6 +19,15 @@ import java.awt.event.ActionEvent;
 import javax.swing.JMenuItem;
 
 import java.awt.CardLayout;
+import javax.swing.JButton;
+import java.awt.GridLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import java.awt.FlowLayout;
+import javax.swing.BoxLayout;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
 
 public class Page extends JFrame
 {
@@ -33,6 +42,9 @@ public class Page extends JFrame
 	
 	private static double screenwidth = dm.getWidth();
 	private static double screenheight = dm.getHeight();
+	private JTextField nameField;
+	private JTextField emailField;
+	private JTextField phoneField;
 
 //Main
 	
@@ -66,6 +78,7 @@ public class Page extends JFrame
 	public Page()
 	{
 		setTitle("Silent Auction");
+		setBounds(100, 100, 550, 350);
 		
 		ImageIcon img = new ImageIcon("assets/logo.png");
 		setIconImage(img.getImage());
@@ -78,6 +91,10 @@ public class Page extends JFrame
 		JMenuItem mntmHome = new JMenuItem("Home");
 
 		menuBar.add(mntmHome);
+		
+		JMenuItem mntmRegister = new JMenuItem("Register");
+
+		menuBar.add(mntmRegister);
 		
 		JMenuItem mntmDonate = new JMenuItem("Donate");
 		menuBar.add(mntmDonate);
@@ -97,6 +114,107 @@ public class Page extends JFrame
 		JPanel homePanel = new JPanel();
 		homePanel.setBackground(Color.BLUE);
 		contentPane.add(homePanel);
+		
+		JPanel registrationPanel = new JPanel();
+		registrationPanel.setBackground(Color.RED);
+		contentPane.add(registrationPanel);
+		registrationPanel.setLayout(new BorderLayout(0, 0));
+		
+		JPanel innerRegPanel = new JPanel();
+		registrationPanel.add(innerRegPanel, BorderLayout.CENTER);
+		GridBagLayout gbl_innerRegPanel = new GridBagLayout();
+		gbl_innerRegPanel.columnWidths = new int[]{0, 0, 0, 0, 0, 0, 0};
+		gbl_innerRegPanel.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gbl_innerRegPanel.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
+		gbl_innerRegPanel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		innerRegPanel.setLayout(gbl_innerRegPanel);
+		
+		JLabel nameLabel = new JLabel("Enter your name:");
+		GridBagConstraints gbc_nameLabel = new GridBagConstraints();
+		gbc_nameLabel.anchor = GridBagConstraints.WEST;
+		gbc_nameLabel.insets = new Insets(0, 0, 5, 5);
+		gbc_nameLabel.gridx = 3;
+		gbc_nameLabel.gridy = 0;
+		innerRegPanel.add(nameLabel, gbc_nameLabel);
+		
+		nameField = new JTextField();
+		GridBagConstraints gbc_nameField = new GridBagConstraints();
+		gbc_nameField.insets = new Insets(0, 0, 5, 0);
+		gbc_nameField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_nameField.gridx = 5;
+		gbc_nameField.gridy = 0;
+		innerRegPanel.add(nameField, gbc_nameField);
+		nameField.setColumns(10);
+		
+		JLabel label = new JLabel(" ");
+		GridBagConstraints gbc_label = new GridBagConstraints();
+		gbc_label.insets = new Insets(0, 0, 5, 0);
+		gbc_label.gridx = 5;
+		gbc_label.gridy = 1;
+		innerRegPanel.add(label, gbc_label);
+		
+		JLabel emailLabel = new JLabel("Enter your email:");
+		GridBagConstraints gbc_emailLabel = new GridBagConstraints();
+		gbc_emailLabel.anchor = GridBagConstraints.WEST;
+		gbc_emailLabel.insets = new Insets(0, 0, 5, 5);
+		gbc_emailLabel.gridx = 3;
+		gbc_emailLabel.gridy = 2;
+		innerRegPanel.add(emailLabel, gbc_emailLabel);
+		
+		emailField = new JTextField();
+		GridBagConstraints gbc_emailField = new GridBagConstraints();
+		gbc_emailField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_emailField.insets = new Insets(0, 0, 5, 0);
+		gbc_emailField.gridx = 5;
+		gbc_emailField.gridy = 2;
+		innerRegPanel.add(emailField, gbc_emailField);
+		emailField.setColumns(10);
+		
+		JLabel label_1 = new JLabel(" ");
+		GridBagConstraints gbc_label_1 = new GridBagConstraints();
+		gbc_label_1.insets = new Insets(0, 0, 5, 0);
+		gbc_label_1.gridx = 5;
+		gbc_label_1.gridy = 3;
+		innerRegPanel.add(label_1, gbc_label_1);
+		
+		JLabel phoneLabel = new JLabel("Enter your phone number:");
+		GridBagConstraints gbc_phoneLabel = new GridBagConstraints();
+		gbc_phoneLabel.anchor = GridBagConstraints.WEST;
+		gbc_phoneLabel.insets = new Insets(0, 0, 5, 5);
+		gbc_phoneLabel.gridx = 3;
+		gbc_phoneLabel.gridy = 4;
+		innerRegPanel.add(phoneLabel, gbc_phoneLabel);
+		
+		phoneField = new JTextField();
+		GridBagConstraints gbc_phoneField = new GridBagConstraints();
+		gbc_phoneField.insets = new Insets(0, 0, 5, 0);
+		gbc_phoneField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_phoneField.gridx = 5;
+		gbc_phoneField.gridy = 4;
+		innerRegPanel.add(phoneField, gbc_phoneField);
+		phoneField.setColumns(10);
+		
+		JLabel label_2 = new JLabel(" ");
+		GridBagConstraints gbc_label_2 = new GridBagConstraints();
+		gbc_label_2.insets = new Insets(0, 0, 5, 0);
+		gbc_label_2.gridx = 5;
+		gbc_label_2.gridy = 5;
+		innerRegPanel.add(label_2, gbc_label_2);
+				
+				JButton btnRegister = new JButton("Register");
+				
+						GridBagConstraints gbc_btnRegister = new GridBagConstraints();
+						gbc_btnRegister.insets = new Insets(0, 0, 5, 0);
+						gbc_btnRegister.anchor = GridBagConstraints.EAST;
+						gbc_btnRegister.gridx = 5;
+						gbc_btnRegister.gridy = 6;
+						innerRegPanel.add(btnRegister, gbc_btnRegister);
+				
+				btnRegister.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						//TODO: Create a new user with ID
+					}
+				});
 		
 		JPanel donatePanel = new JPanel();
 		donatePanel.setBackground(Color.GREEN);
@@ -118,21 +236,29 @@ public class Page extends JFrame
 		itemPanel.setBackground(Color.PINK);
 		contentPane.add(itemPanel);
 		
-		JPanel registrationPanel = new JPanel();
-		registrationPanel.setBackground(Color.RED);
-		contentPane.add(registrationPanel);
 		
 		
-		
-		pack();
+		//pack();
 		setLocation((int)(screenwidth - getWidth()) / 2, (int)(screenheight - getHeight()) / 2);
 		
 		mntmHome.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				donatePanel.setVisible(false);
 				generalStatsPanel.setVisible(false);
+				registrationPanel.setVisible(false);
 				
 				homePanel.setVisible(true);
+			}
+		});
+		
+		mntmRegister.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				homePanel.setVisible(false);
+				donatePanel.setVisible(false);
+				generalStatsPanel.setVisible(false);
+				
+				registrationPanel.setVisible(true);
+				
 			}
 		});
 		
@@ -141,6 +267,7 @@ public class Page extends JFrame
 			public void actionPerformed(ActionEvent e) {
 				homePanel.setVisible(false);
 				generalStatsPanel.setVisible(false);
+				registrationPanel.setVisible(false);
 				
 				donatePanel.setVisible(true);
 			}
@@ -150,6 +277,7 @@ public class Page extends JFrame
 			public void actionPerformed(ActionEvent e) {
 				homePanel.setVisible(false);
 				donatePanel.setVisible(false);
+				registrationPanel.setVisible(false);
 				
 				generalStatsPanel.setVisible(true);
 			}
@@ -161,7 +289,4 @@ public class Page extends JFrame
 			}
 		});
 	}
-	
-//Methods
-
 }
