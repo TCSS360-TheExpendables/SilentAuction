5301c8ea3d28498fc892d28b18b429888ef09de5
Chris Ottersen <christopherottersen@gmail.com>
2015-05-23 19:43:29 -0700

    back_end
    commit

--- SilentAuction/src/backend/Contribution.java
+++ SilentAuction/src/backend/Contribution.java
@@ -1,6 +1,23 @@
 package backend;
 
-public interface Contribution
+
+public abstract class Contribution
 {
   
+  private int bidderId;
+  private long timestamp;
+  public Contribution(int bidderId)
+  {
+    this.bidderId = bidderId;
+    this.timestamp = System.currentTimeMillis();
+    
+  }
+  public int getBidder()
+  {
+    return this.bidderId;
+  }
+  public long getTimeStamp()
+  {
+    return this.timestamp;
+  }
 }
