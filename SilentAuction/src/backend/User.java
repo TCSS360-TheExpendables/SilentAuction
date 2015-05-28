c567adac700e39f27430c08806097ab502ec5402
Robearification <robertcgillis1993@gmail.com>
2015-05-28 16:16:15 -0700

    Bidder, Donor, User

--- SilentAuction/src/backend/User.java
+++ SilentAuction/src/backend/User.java
@@ -1,20 +1,21 @@
 package backend;
 
 import java.util.EventListener;
-import java.util.LinkedList;
-import java.util.List;
 
-public class User implements EventListener
+abstract class User implements EventListener
 {
-  public final String name;
-  private final List<Bid> bids;
-  private final List<Contribution> donations;
+  protected final String name;
+  protected final int ID;
+  protected final String email;
+  protected final String phone;
+
   
-  public User(final String name)
+  public User(final String name, final String email, final String phone)
   {
     this.name = name;
-    this.bids = new LinkedList<Bid>();
-    this.donations = new LinkedList<Contribution>();
+    this.ID = hashCode();
+    this.email = email;
+    this.phone = phone;
     
   }
   @Override
@@ -22,21 +23,6 @@ public class User implements EventListener
   {
     return this.name.hashCode();
   }
-  
-  public void placeBid(final Item item, final double amount)
-  {
-    Bid bid = new Bid(amount, this.name.hashCode());
-    item.addBid(bid);
-    this.bids.add(bid);
-  }
-  
-  public void donate(final double cash)
-  {
-    this.donate(new Cash(cash));
-  }
-  public void donate(final Contribution contribution)
-  {
-    this.donations.add(contribution);
-  }
+ 
   
 }
